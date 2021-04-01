a94dc037e1176008e086af24f329aa347c149b55
Lazy-Lad <code.with.lazylad@gmail.com>
2021-04-01 22:12:39 +0530

    almost complete. BUGs check <wip>

--- 1st_one/script.js
+++ 1st_one/script.js
@@ -8,7 +8,9 @@ var b = "";
 var op = '';
 var numLIst= [];
 var operatorList = []
+var isCalculated = false
 function getValue(getNum){ 
+    b = b + getNum;
     a = a+getNum;
     setValue(a);
 }
@@ -18,33 +20,88 @@ function deleteSingleDigit(digit){
 }
 
 function delSingleDigit(){
-    deleteSingleDigit(a);
+    if(!isCalculated){
+        deleteSingleDigit(a)
+        operatorList.pop()
+    }
 }
 
 function allClear(){
-    a = "";
-    numLIst = [];
-    operatorList=[];
-    setValue('');
+    a = ""
+    b = ''
+    numLIst = []
+    operatorList=[]
+    setValue("0000000")
 }
 function pushOperator(operator){
-    numLIst.push(a);
+
+    if(b!= ""){
+        numLIst.push(b)
+        b = ""
+    }
     operatorList.push(operator);
     a = a+ operator;
-    b = b +  numLIst.slice(-1) + operatorList.slice(-1);
-    setValue(b)
+    setValue(a)
 }
 function equals(){
     op = a.slice(-1);
-    if(op === '-' || op === '+' || op === 'X' || op === '/' ){
+    if(op === '-' || op === '+' || op === '*' || op === '/' ){
         setValue('Invalid Opeartion');
+        // allClear();
     }
     else{
-        numLIst.push(a);
+        numLIst.push(b)
+        console.log(op)
+        console.log(numLIst)
+        console.log(operatorList)
+        setValue(calculate())
+        isCalculated = true
     }
-    console.log(op)
-    console.log(numLIst)
-    console.log(operatorList)
+
+}
+
+function calculate(){
+    // B - Bracket
+    // O - of
+    // D - Division
+    // M - Multiplication
+    // A - Addition
+    // S - Subtraction
+    var i = 0;
+    var BODMAS_list = ["/","*","+","-"]
+
+    while(operatorList.length>0){
+        console.log(operatorList.indexOf(BODMAS_list[i]))
+        while(operatorList.indexOf(BODMAS_list[i]) != -1){
+            let j = operatorList.indexOf(BODMAS_list[i])
+            let a = parseFloat(numLIst[j])
+            let b = parseFloat(numLIst[j+1])
+            let op = operatorList[j]
+            numLIst[j] = operate(a,b,op)
+            numLIst.splice(j+1,1)
+            operatorList.splice(j,1)
+            console.log("*********")
+            console.log(operatorList)
+            console.log(numLIst)
+        }
+        i++
+        
+    }
+
+   return numLIst[0]
+}
+
+function operate(a,b,op){
+    switch (op){
+        case '/':
+            return a/b
+        case '*':
+            return a*b
+        case '+':
+            return a+b
+        default:
+            return a-b
+    }
 }
 
 
